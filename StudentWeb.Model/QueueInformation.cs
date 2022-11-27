344615c405dd90cdbfedafe2a5bb99f722e02327
MYlindaxia <2444501319@qq.com>
2022-11-27 10:28:08 +0800

    first commit

---
+++ StudentWeb.Model/QueueInformation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using StudentWeb.Common;
+
+namespace StudentWeb.Model
+{
+	/// <summary>
+	/// QueueInformation
+	/// </summary>
+    [Serializable]
+	public class QueueInformation
+	{
+		/// <summary>
+		/// UserName
+        /// </summary>
+        [FieldName("UserName")]		
+		public string UserName
+		{
+			get;
+			set;
+		}
+        
+		/// <summary>
+		/// UserNumber
+        /// </summary>
+        [FieldName("UserNumber")]		
+		public string UserNumber
+		{
+			get;
+			set;
+		}
+        
+		/// <summary>
+		/// UserID
+        /// </summary>
+        [FieldName("UserID")]		
+		public string UserID
+		{
+			get;
+			set;
+		}
+        
+        public QueueInformation Clone()
+        {
+            return (QueueInformation)this.MemberwiseClone();
+        }
+        
+        public QueueInformation()
+        {
+            UserName = string.Empty;
+            UserNumber = string.Empty;
+            UserID = string.Empty;
+        }
+	}
+}
